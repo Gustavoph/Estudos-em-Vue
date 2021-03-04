9b6c159635461172678adbfba9af04d4d4678f38
Gustavoph <gusta.coliveira@gmail.com>
2021-03-04 15:43:35 -0300

    Vue cli

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ nome }} {{ sobrenome }}
+  </div>  
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      nome: 'Gustavo Gabriel',
+      sobrenome: 'Campos de Oliveira'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
